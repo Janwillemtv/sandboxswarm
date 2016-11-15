1a994628ec17f75a0ce4a898f9e5c3890fd8dcae
Raoul Fasel <raoul@fasel.nl>
2016-11-15 15:50:01 +0100

    fixed

--- linux_startup.sh
+++ linux_startup.sh
@@ -1,10 +1,12 @@
 #if you run this hacky script in rc.local for example the sandbox will start at boot.
-if [ $(/usr/local/bin/git status) == *"Your branch is up-to-date with"* ] 
+if [ $(git status) == *"Your branch is up-to-date with"* ] 
 then
+    echo "update needed"
     git fetch --all
     git reset --hard origin/master
     make
 fi
 
+echo "#################- RUNNING SANDBOX -#############################"
 xinit &
 ./bin/sandboxSwarmDebug
