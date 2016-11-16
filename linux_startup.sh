e462b0f67bcbb6ebaf4f3aa9b6c78ba32c2b3831
Raoul Fasel <RaoulFasel@users.noreply.github.com>
2016-11-16 11:25:02 +0100

    Update linux_startup.sh

--- linux_startup.sh
+++ linux_startup.sh
@@ -1,5 +1,5 @@
 #if you run this hacky script in rc.local for example the sandbox will start at boot.
-if [ $(git status) == *"Your branch is up-to-date with"* ] 
+if [[ $(git status) == *"Your branch is up-to-date with"* ]] 
 then
     echo "update needed"
     git fetch --all
