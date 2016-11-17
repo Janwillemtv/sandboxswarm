6c8979f7cf81e697fa10418509f8bd083ae4c022
Raoul Fasel <RaoulFasel@users.noreply.github.com>
2016-11-17 17:57:36 +0100

    Update linux_startup.sh

--- linux_startup.sh
+++ linux_startup.sh
@@ -2,10 +2,11 @@
 exprot DISPLAY=:0
 cd /home/janwillem/sandbox/of_v0.9.7_linux64_release/apps/myApps/sandboxswarm/
 xrandr --output VGA-1 --mode 1024x768
-if [[ $(git status) != *"Your branch is up-to-date with"* ]] 
+git fetch --all
+if [[ $(git status) != *"Your branch is behind"* ]] 
 then
     echo "update needed"
-    git fetch --all
+  
     git reset --hard origin/master
     alias gcc='clang'
     alias g++='clang++'
@@ -14,5 +15,7 @@ fi
 
 echo "#################- RUNNING SANDBOX -#############################"
 xinit &
+
 sleep 2
+xset s off
 ./bin/sandboxswarm
