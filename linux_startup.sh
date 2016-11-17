0c0e6a2e6be2cd2cd8031cf4f32b3e14ca74888b
Raoul Fasel <RaoulFasel@users.noreply.github.com>
2016-11-17 10:59:22 +0100

    Update linux_startup.sh
    comparison was wrong and bin name

--- linux_startup.sh
+++ linux_startup.sh
@@ -1,5 +1,5 @@
 #if you run this hacky script in rc.local for example the sandbox will start at boot.
-if [[ $(git status) == *"Your branch is up-to-date with"* ]] 
+if [[ $(git status) != *"Your branch is up-to-date with"* ]] 
 then
     echo "update needed"
     git fetch --all
@@ -11,4 +11,4 @@ fi
 
 echo "#################- RUNNING SANDBOX -#############################"
 xinit &
-./bin/sandboxSwarmDebug
+./bin/sandboxswarm
