f02d98afdb5911719079f92b2cf965ed35ddeed3
Raoul Fasel <RaoulFasel@users.noreply.github.com>
2017-03-22 11:59:42 +0100

    Update linux_startup.sh
    reboot if sandbox crashes. This is really ugly :)

--- linux_startup.sh
+++ linux_startup.sh
@@ -21,3 +21,4 @@ xset s off         # don't activate screensaver
 xset -dpms         # disable DPMS (Energy Star) features.
 xset s noblank     # don't blank the video device
 ./bin/sandboxswarm
+reboot
